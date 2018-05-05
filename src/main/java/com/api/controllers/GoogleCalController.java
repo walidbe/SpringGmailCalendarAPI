fb119ad8350d70d454d0db211e163e123fee508e
walid <walid@192.168.2.17>
2018-05-05 13:11:35 -0400

    Google Rest API POST an Event

--- src/main/java/com/api/controllers/GoogleCalController.java
+++ src/main/java/com/api/controllers/GoogleCalController.java
@@ -1,6 +1,8 @@
 package com.api.controllers;
 
 import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashSet;
@@ -33,6 +35,9 @@ import com.google.api.client.util.DateTime;
 import com.google.api.services.calendar.Calendar.Events;
 import com.google.api.services.calendar.CalendarScopes;
 import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventAttendee;
+import com.google.api.services.calendar.model.EventDateTime;
+import com.google.api.services.calendar.model.EventReminder;
 
 @Controller
 public class GoogleCalController {
@@ -64,13 +69,66 @@ public class GoogleCalController {
 	}
 
 	
+	@RequestMapping(value = "/login/google", method = RequestMethod.POST)
+	public ResponseEntity<String> updateEvent() throws IOException, GeneralSecurityException {
+		
+		Event event = new Event()
+			    .setSummary("BEZZAOUI WALIDDDDDD 2018")
+			    .setLocation("1000 RUE NOTRE DAME")
+			    .setDescription("BLA BLA BLA BLA BLA");
+		DateTime startDateTime = new DateTime("2018-05-06T09:00:00-07:00");
+		EventDateTime start = new EventDateTime()
+		    .setDateTime(startDateTime)
+		    .setTimeZone("America/Los_Angeles");
+		event.setStart(start);
 
+		DateTime endDateTime = new DateTime("2018-05-07T17:00:00-07:00");
+		EventDateTime end = new EventDateTime()
+		    .setDateTime(endDateTime)
+		    .setTimeZone("America/Los_Angeles");
+		event.setEnd(end);
+
+		String[] recurrence = new String[] {"RRULE:FREQ=DAILY;COUNT=2"};
+		event.setRecurrence(Arrays.asList(recurrence));
+
+		EventAttendee[] attendees = new EventAttendee[] {
+		    new EventAttendee().setEmail("walidrift@gmail.com"),
+		    new EventAttendee().setEmail("walid.bezzaoui@hotmail.fr"),
+		};
+		event.setAttendees(Arrays.asList(attendees));
+
+		EventReminder[] reminderOverrides = new EventReminder[] {
+		    new EventReminder().setMethod("email").setMinutes(24 * 60),
+		    new EventReminder().setMethod("popup").setMinutes(10),
+		};
+		Event.Reminders reminders = new Event.Reminders()
+		    .setUseDefault(false)
+		    .setOverrides(Arrays.asList(reminderOverrides));
+		event.setReminders(reminders);
+		
+		String calendarId = "primary";
+		TokenResponse response = new TokenResponse().setAccessToken("ya29.GluyBW-HAeBFYmP22i3Rhywun-4_LpKR-mQMet-fxHfj_vfst1eUGXhzXyIPdxWfg2uf6Ynm2ssy1XlbrWYRSb57wVgBHP6OM_zrx7JV8FN8n5pEAVfyORVmAOV3").setExpiresInSeconds((long) 3600).setRefreshToken("1/N8MI2-yqRYRs5HoNq67E7VySDm7u_xl-rAfa7HtOsI-9wiYW47FTt8XSHHg8o0De").setTokenType("Bearer");
+		Details web = new Details();
+		web.setClientId(clientId);
+		web.setClientSecret(clientSecret);
+		clientSecrets = new GoogleClientSecrets().setWeb(web);
+		httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+		flow = new GoogleAuthorizationCodeFlow.Builder(httpTransport, JSON_FACTORY, clientSecrets,
+				Collections.singleton(CalendarScopes.CALENDAR)).build();
+		credential = flow.createAndStoreCredential(response, "userID");
+		client = new com.google.api.services.calendar.Calendar.Builder(httpTransport, JSON_FACTORY, credential)
+				.setApplicationName(APPLICATION_NAME).build();
+		event = client.events().insert(calendarId, event).execute();
+		System.out.printf("Event created: %s\n", event.getHtmlLink());
+		return new ResponseEntity<>(event.getHtmlLink(), HttpStatus.OK);
+		
+	}
 	@RequestMapping(value = "/login/google", method = RequestMethod.GET)
 	public ResponseEntity<String> oauth2Callback() {
 		com.google.api.services.calendar.model.Events eventList;
 		String message;
 		try {
-			TokenResponse response = new TokenResponse().setAccessToken("ya29.GluyBRtDEWQQEPaHrtXv2iFtTYkVKryA2nBaDXIU98CcqlCXimiZq2D9OUPwYNeG9hZA94nG9cVXCQ0qtVn6C2xjx4xYtczgQeoTimv0Z5Jno6ZK4-JOVxqTgmD8").setExpiresInSeconds((long) 3600).setRefreshToken("1/N8MI2-yqRYRs5HoNq67E7VySDm7u_xl-rAfa7HtOsI-9wiYW47FTt8XSHHg8o0De").setTokenType("Bearer");
+			TokenResponse response = new TokenResponse().setAccessToken("ya29.GluyBW-HAeBFYmP22i3Rhywun-4_LpKR-mQMet-fxHfj_vfst1eUGXhzXyIPdxWfg2uf6Ynm2ssy1XlbrWYRSb57wVgBHP6OM_zrx7JV8FN8n5pEAVfyORVmAOV3").setExpiresInSeconds((long) 3600).setRefreshToken("1/N8MI2-yqRYRs5HoNq67E7VySDm7u_xl-rAfa7HtOsI-9wiYW47FTt8XSHHg8o0De").setTokenType("Bearer");
 			Details web = new Details();
 			web.setClientId(clientId);
 			web.setClientSecret(clientSecret);
